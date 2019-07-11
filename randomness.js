0331b34648f115b798826727a566576f3bb8d740
jalenwASC6 <jalenw44@nycischool.org>
2019-07-11 13:53:25 -0400

    ^x

---
+++ randomness.js
@@ -0,0 +1,33 @@
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert"
+];
+/////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
+
+function randomNumber() {
+console.log(Math.random() * 21 + 1);
+}
+randomNumber()
+function randomLetter() {
+console.log(alphabet[Math.floor(Math.random) * (alphabet.length)]);
+}
+randomLetter()
