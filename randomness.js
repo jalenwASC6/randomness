da0320d66ff789dc4b046014107e7d6ce69cd0cb
jalenwASC6 <jalenw44@nycischool.org>
2019-07-11 19:32:17 -0400

    Randomness

--- randomness.js
+++ randomness.js
@@ -27,7 +27,18 @@ function randomNumber() {
 console.log(Math.random() * 21 + 1);
 }
 randomNumber()
+
 function randomLetter() {
 console.log(alphabet[Math.floor(Math.random()*alphabet.length)]);
 }
 randomLetter()
+
+function randomFoods() {
+console.log(foods[Math.floor(Math.random()*foods.length)]);
+}
+randomFoods()
+
+function randomStudent() {
+console.log(students[Math.floor(Math.random()*students.length)]);
+}
+randomStudent()
